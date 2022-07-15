8065d91fac4d2ca9965bb1dc3f2e8351b345e6f6
seonhop <78877741+seonhop@users.noreply.github.com>
2022-07-15 12:12:13 -0400

    #4.8 Nested Routes Part Two

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
+import Chart from "./routes/Chart";
+import Price from "./routes/Price";
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/:coinId" element={<Coin />} />
+                <Route path="/:coinId" element={<Coin />}>
+                    <Route path="chart" element={<Chart />} />
+                    <Route path="price" element={<Price />} />
+                </Route>
+
                 <Route path="/" element={<Coins />} />
             </Routes>
         </BrowserRouter>
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,10 @@
-import { useParams, useLocation } from "react-router-dom";
+import {
+    useParams,
+    useLocation,
+    Outlet,
+    Link,
+    useMatch,
+} from "react-router-dom";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { IInfoData } from "../defs/IInfoData";
@@ -31,6 +37,51 @@ const Loader = styled.span`
     display: block;
 `;
 
+const Overview = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    span:first-child {
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    }
+`;
+
+const Description = styled.p`
+    margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 7px 0px;
+    border-radius: 10px;
+    color: ${(props) =>
+        props.isActive ? props.theme.accentColor : props.theme.textColor};
+    a {
+        display: block;
+    }
+`;
+
 interface RouteState {
     state: {
         name: string;
@@ -45,6 +96,8 @@ function Coin() {
     const { state } = useLocation() as RouteState;
     const [info, setInfo] = useState<IInfoData>({});
     const [priceInfo, setPriceInfo] = useState<IPriceData>({});
+    const priceMatch = useMatch("/:coinId/price");
+    const chartMatch = useMatch("/:coinId/chart");
     useEffect(() => {
         (async () => {
             const infoData = await (
@@ -57,14 +110,61 @@ function Coin() {
             console.log(priceData);
             setInfo(infoData);
             setPriceInfo(priceData);
+            setLoading(false);
         })();
-    }, []);
+    }, [coinId]);
     return (
         <Container>
             <Header>
-                <Title>{state?.name || "Loading"}</Title>
+                <Title>
+                    {state?.name
+                        ? state.name
+                        : loading
+                        ? "Loading..."
+                        : info?.name}
+                </Title>
             </Header>
-            {loading ? <Loader>Loading...</Loader> : null}
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>${info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source:</span>
+                            <span>{info?.open_source ? "Yes" : "No"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Suply:</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Tabs>
+                        <Tab isActive={chartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>Chart</Link>
+                        </Tab>
+                        <Tab isActive={priceMatch !== null}>
+                            <Link to={`/${coinId}/price`}>Price</Link>
+                        </Tab>
+                    </Tabs>
+
+                    <Outlet />
+                </>
+            )}
         </Container>
     );
 }
